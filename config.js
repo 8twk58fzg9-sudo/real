b4da1e5700d0d3205826a8a0141e427d6697d238
8twk58fzg9-sudo <8twk58fzg9@privaterelay.appleid.com>
2026-06-15 23:55:51 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // This file may be uploaded to GitHub Pages only with public/publishable values.
 // Never paste service_role, SMTP, GoPay, iDoklad, provider private, or other secret keys here.
 window.COMPUTRAX_CONFIG = Object.freeze({
-  SUPABASE_ANON_KEY: 'PASTE_PUBLIC_SUPABASE_ANON_KEY_HERE',
+  SUPABASE_ANON_KEY: 'sb_publishable_x6sSWhw3on9bi_C1EQdTCg_nz09VWoX',
   EMAIL_ENDPOINT: ''
 });
